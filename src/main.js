28b5c5b2fc0366c233826f85c5ecd33adccecc24
lnn520 <335341543@qq.com>
2020-10-15 11:41:33 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,25 @@
+
+// const api  = jQuery('.test')
+// api.addClass('red').addClass('blue').addClass('yellow')//链式操作
+
+// jQuery('.test1')
+// .addClass('red')
+// .addClass('blue')
+// .addClass('yellow')//链式操作
+
+// obj.fn(p1)
+// obj.fn.call(obj,p1)
+
+// const elements = document.querySelectorAll('.test')[0]
+
+// elements.classList.add('black')
+
+
+console.log(jQuery('.test'))
+
+const x =jQuery('.test')
+
+// x.each((div)=>console.log(div))
+
+x.children().print()
+
